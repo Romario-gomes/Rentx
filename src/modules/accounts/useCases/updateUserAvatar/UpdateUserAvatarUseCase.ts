f0348a10cd4407fa8bc6823e360900eb82a87da0
Romario Work <romario.g@estrela10.com.br>
2022-03-31 21:07:56 -0300

    Feat: add Remove avatar already exists

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,6 +1,8 @@
 import { inject, injectable } from "tsyringe";
 import { IUsersRespository } from "../../repositories/IUsersRepository";
 
+import { deleteFile } from "../../../../utils/file";
+
 interface IRequest {
     user_id: string;
     avatar_file: string;
@@ -14,6 +16,10 @@ class UpdateUserAvatarUseCase{
     async execute({ user_id, avatar_file }: IRequest): Promise<void> {
         const user = await this.usersRepository.findById(user_id);
     
+        if(user.avatar){
+            await deleteFile(`./tmp/avatar/${user.avatar}`);
+        }
+
         user.avatar = avatar_file;
 
         await this.usersRepository.create(user);
