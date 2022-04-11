efce2de2f12da3c6cfc725e5bb25d388b3f159d3
Romario Alves <romariogn10@gmail.com>
2022-04-11 07:00:41 -0300

    Feat (Categories): Add Repository in memory

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,5 +1,16 @@
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 describe("create Category", () => {
+    let createCategoryUseCase: CreateCategoryUseCase;
+    let categoriesRepositoryInMemory: CategoriesRepositoryInMemory
+
+    beforeEach( () => {
+        let categoriesRepositoryInMemory = new CategoriesRepositoryInMemory;
+        let createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory);
+
+    });
+
     it("Should be able create a new category", () => {
         const soma = 2 + 2;
 
