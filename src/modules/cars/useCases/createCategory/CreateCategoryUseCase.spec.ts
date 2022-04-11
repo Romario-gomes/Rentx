b6d5b120fc0e11b73ac7ae628b831add2b24661a
Romario Alves <romariogn10@gmail.com>
2022-04-11 07:28:42 -0300

    Feat (CreateCategory): add Test

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -14,7 +14,7 @@ describe("create Category", () => {
     it("Should be able create a new category", async () => {
         const category = {
             name: "Category test",
-            description: "Category description Test a",
+            description: "Category description Test",
         }
 
 
