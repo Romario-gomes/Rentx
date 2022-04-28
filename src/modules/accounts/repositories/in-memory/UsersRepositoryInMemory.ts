205ddac03965d9676ec1b455ffe6fd71b0ab0c86
Romario Alves <romariogn10@gmail.com>
2022-04-28 11:07:10 -0300

    feat (CreateCars): add Migration

--- src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -1,5 +1,6 @@
-import { ICreateUserDTO } from "../../../../../dtos/ICreateUserDTO";
-import { User } from "../../../entities/User";
+
+import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
+import { User } from "@modules/accounts/infra/typeorm/entities/User";
 import { IUsersRespository } from "../IUsersRepository";
 
 class UsersRepositoryInMemory implements IUsersRespository {
