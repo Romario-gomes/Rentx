6ae4dd71d62003e3f3e9f4e42e19f8068d5f60b1
Romario Alves <romariogn10@gmail.com>
2022-06-02 17:47:01 -0300

    feat(Refresh token): add UsersTokens repository

---
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -0,0 +1,31 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateUserTokenDTO } from "@modules/accounts/dtos/ICreateUserTokenDTO";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+
+import { UserTokens } from "../entities/UserTokens";
+
+class UsersTokensRepository implements IUsersTokensRepository {
+  private repository: Repository<UserTokens>;
+
+  constructor() {
+    this.repository = getRepository(UserTokens);
+  }
+  async create({
+    user_id,
+    expires_date,
+    refresh_token,
+  }: ICreateUserTokenDTO): Promise<UserTokens> {
+    const userToken = this.repository.create({
+      user_id,
+      expires_date,
+      refresh_token,
+    });
+
+    await this.repository.save(userToken);
+
+    return userToken;
+  }
+}
+
+export { UsersTokensRepository };
