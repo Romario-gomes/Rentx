688f8abf0bc7d61a064a6749c0537c5b90e15b3b
Romario Alves <romariogn10@gmail.com>
2022-06-10 17:06:00 -0300

    Feat(Update Rma): Add controller, Routes and UseCase

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   secret_token: "a95e14185b20a69b71794c47abcfec30",
-  expires_in_token: "15m",
+  expires_in_token: "15min",
   secret_refresh_token: "d24f1153e2c49374a5cb2ee50deffb66",
   expires_in_refresh_token: "30d",
   expires_refresh_token_days: 30,
--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -1,23 +1,21 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
+
 import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
 
 class UpdateUserAvatarController {
-    async handle(request: Request, response: Response): Promise<Response> {
-        const { id } = request.user;
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.user;
 
-        const avatar_file = request.file.filename;
-        // Receber arquivo
+    const avatar_file = request.file.filename;
+    // Receber arquivo
 
-        const updateUserAvatarUseCase = container.resolve(
-            UpdateUserAvatarUseCase
-        )
+    const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
 
-        updateUserAvatarUseCase.execute({ user_id: id, avatar_file });
+    updateUserAvatarUseCase.execute({ user_id: id, avatar_file });
 
-        return response.status(204).send();
-    }
-
+    return response.status(204).send();
+  }
 }
 
 export { UpdateUserAvatarController };
